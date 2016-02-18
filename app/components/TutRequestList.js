cfe6e7ea0bd55bdde932359eb38ecbd87c47d341
Alex Booker <alexbooker@fastmail.im>
2016-02-18 21:37:35 +0000

    Make redability tweaks to TutRequestList.js

--- app/components/TutRequestList.js
+++ app/components/TutRequestList.js
@@ -1,20 +1,34 @@
-import React from 'react';
 import {Link, IndexLink} from 'react-router';
 import TutRequestListItem from './TutRequestListItem.js';
 
-function TutRequestList({tutRequests, moreTutRequests, fetchingTutRequests, onFetchMoreClicked}) {
+function SortOptions() {
+  return (
+    <ul>
+      <li><IndexLink to="/">Latest</IndexLink></li>
+      <li><Link to="/popular">Popular</Link></li>
+    </ul>
+  );
+}
+
+function FetchMoreButton({onFetchMoreClicked, moreTutRequests, fetchingTutRequests}) {
+  return (
+    <button
+      disabled={moreTutRequests || fetchingTutRequests}
+      onClick={onFetchMoreClicked}>
+      { fetchingTutRequests ? 'Fetching...' : 'Fetch More' }
+    </button>
+  );
+}
+
+function TutRequestList(props) {
+  const {tutRequests} = props;
   return (
     <div>
-      <ul>
-        <li><IndexLink to="/">Latest</IndexLink></li>
-        <li><Link to="/popular">Popular</Link></li>
-      </ul>
+      <SortOptions />
       <ul>
         {tutRequests.map((tutRequest, i) => <TutRequestListItem key={i} {...tutRequest} />)}
       </ul>
-      <button disabled={moreTutRequests || fetchingTutRequests} onClick={onFetchMoreClicked}>
-        { fetchingTutRequests ? 'Fetching...' : 'Fetch More' }
-      </button>
+      <FetchMoreButton {...props}/>
     </div>
   );
 }
